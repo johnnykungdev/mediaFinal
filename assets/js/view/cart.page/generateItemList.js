c4ed7d9c4cda280cfc574dd3a64274eec659194b
Che-Yi Kung <kjohnathan.engi@gmail.com>
2021-01-12 14:52:10 -0800

    add list item method

--- assets/js/view/cart.page/generateItemList.js
+++ assets/js/view/cart.page/generateItemList.js
@@ -1,5 +1,31 @@
-function generateItemList(itemList) {
-    // itemList.forEach((item) => {
-    //     const ne
-    // })
+class cartItems {
+    constructor() {
+        
+    }
+    listItems(itemsArr) {
+        const itemsTable = document.querySelector('.itemsTable')
+        itemsArr.forEach(item => {
+            //{ item: '', itemPrice: '', itemAmount: '', itemSubprice: '' }
+            const newItemTr = document.createElement('tr')
+            for (let infoType in item) {
+                const infoTd = document.createElement('td')
+                infoTd.classList.add(`${infoType}`)
+                if (infoType === 'itemAmount') {
+                    infoTd.innerHTML = `<input value=${item[infoType]} />`
+                    infoTd.addEventListener('change', function() {
+                        //add changeItemAmount
+                    })
+                } else {
+                    infoTd.innerHTML = item[infoType]
+                }
+                newItemTr.appendChild(infoTd)
+            }
+            itemsTable.appendChild(newItemTr)
+        })
+    }
+    changeItemAmount(newAmount) {
+        //change the amount of selected item in the itemsArr
+    }
 }
+
+export default cartItems
