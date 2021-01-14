06121fde899f293847ba045c09c0e2d8c433bf98
Che-Yi Kung <kjohnathan.engi@gmail.com>
2021-01-13 20:59:13 -0800

    update add item function

--- assets/js/view/cart.page/CartItems.js
+++ assets/js/view/cart.page/CartItems.js
@@ -1,54 +1,53 @@
 class CartItems {
     constructor(itemsArray) {
         this.itemsArray = itemsArray
+        this.tableBody = document.querySelector('.items-table-body')
     }
     changeItemAmount(itemId, newAmount) {
         //change the amount of selected item in the itemsArr
-        console.log(newAmount)
-        console.log(this.itemsArray)
         const changedItem = this.itemsArray.find(item => item.id === itemId)
-        console.log(changedItem)
         changedItem.itemAmount = newAmount
         changedItem.itemSubtotal = changedItem.itemPrice * changedItem.itemAmount
         const newItemSubTotal = document.getElementById(`amountInput-${itemId}`)
-        console.log(newItemSubTotal)
         newItemSubTotal.innerHTML = changedItem.itemSubtotal
+    }
+    //item is a object containing: item, itemPrice, itemAmount, itemSubtotal
+    buildItemTr(item) {
+        const newItemTr = document.createElement('tr')
+        for (let infoType in item) {
+            const infoTd = document.createElement('td')
+            infoTd.classList.add(`${infoType}`)
+            if (infoType === 'itemAmount') {
+                const amountInput = document.createElement('input')
+                amountInput.value = item[infoType]
+                amountInput.addEventListener('input', function(e) {
+                    return _this.changeItemAmount(item.id, e.target.value)
+                })
+                infoTd.appendChild(amountInput)
+            } else if (infoType === 'itemSubtotal') {
+                infoTd.innerHTML = item[infoType]
+                infoTd.id = `amountInput-${item.id}`
+            } else if (infoType !== 'id') {
+                infoTd.innerHTML = item[infoType]
+            }
+            newItemTr.appendChild(infoTd)
+        }
+        const removeButton = document.createElement('div')
+        removeButton.innerHTML = '<u>remove</u>'
+        removeButton.classList.add('removeButton')
+        const removeButtonColumn = document.createElement('td')
+        removeButtonColumn.appendChild(removeButton)
+        newItemTr.appendChild(removeButtonColumn)
+        console.log(this)
+        this.tableBody.appendChild(newItemTr)
+        console.log(this.tableElement)
     }
     listItems() {
         const itemsTable = document.querySelector('.itemsTable')
-        console.log(this)
         const _this = this
         this.itemsArray.forEach(item => {
             //{ item: '', itemPrice: '', itemAmount: '', itemSubprice: '' }
-            const newItemTr = document.createElement('tr')
-
-            //**fix item object to class later**
-            for (let infoType in item) {
-                const infoTd = document.createElement('td')
-                infoTd.classList.add(`${infoType}`)
-                if (infoType === 'itemAmount') {
-                    const amountInput = document.createElement('input')
-                    amountInput.value = item[infoType]
-                    amountInput.addEventListener('input', function(e) {
-                        return _this.changeItemAmount(item.id, e.target.value)
-                    })
-                    infoTd.appendChild(amountInput)
-                } else if (infoType === 'itemSubtotal') {
-                    infoTd.innerHTML = item[infoType]
-                    infoTd.id = `amountInput-${item.id}`
-                } else if (infoType !== 'id') {
-                    infoTd.innerHTML = item[infoType]
-                }
-                newItemTr.appendChild(infoTd)
-            }
-            const removeButton = document.createElement('div')
-            removeButton.innerHTML = '<u>remove</u>'
-            removeButton.classList.add('removeButton')
-            const removeButtonColumn = document.createElement('td')
-            removeButtonColumn.appendChild(removeButton)
-            newItemTr.appendChild(removeButtonColumn)
-
-            itemsTable.appendChild(newItemTr)
+            this.buildItemTr(item)
         })
     }
     displayOrderTotal() {
@@ -58,11 +57,11 @@ class CartItems {
     }
     addItem(newItem) {
         this.itemsArray.push(newItem)
+        return this.buildItemTr(newItem)
     }
     deleteItem(itemId) {
         const deletedItemIndex = this.itemsArray.findIndex(item => item.id === itemId)
         this.itemsArray.splice(deletedItemIndex, 1)
-        console.log(deletedItemIndex)
     }
 }
 
--- assets/js/view/cart.page/cart.page.js
+++ assets/js/view/cart.page/cart.page.js
@@ -6,3 +6,9 @@ console.log(cartItems)
 
 cartItems.listItems(stubData.cartItems)
 cartItems.displayOrderTotal()
+
+const addItemBtn = document.querySelector('.add-item-btn')
+addItemBtn.addEventListener('click', function() {
+    const newItem = { item: 'new item', itemPrice: 3, itemAmount: 1, itemSubtotal: 3, id:3 }
+    return cartItems.addItem(newItem)
+})
